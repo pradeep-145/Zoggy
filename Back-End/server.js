605a7f22c24a8495d51d2efee665ec40a44fde12
pradeep sakthi <117750940+pradeep-145@users.noreply.github.com>
2024-05-31 21:30:23 +0530

    node mailer

--- Back-End/server.js
+++ Back-End/server.js
@@ -22,30 +22,37 @@ app.post('/register',(req,res)=>{
     User.create(req.body).then(result=>res.json(result)).catch(err=>res.json(err))
 })
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: `${process.env.EMAIL}`,
-      pass: process.env.EMAIL_PASSWORD
-    }
-  });
-  
-  var mailOptions = {
-    from: process.env.EMAIL,
-    to: otpemail,
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+app.post('/',(req,res)=>{
 
-app.post('/login',(req,res)=>{
+    console.log(process.env.EMAIL)
+    console.log(process.env.EMAIL_PASSWORD)
+    var transporter = nodemailer.createTransport({
+        host:'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.APP_PASSWORD
+        }
+    });
+    
+    var mailOptions = {
+        from: process.env.EMAIL,
+        to: 'chitrasridharan1980@gmail.com',
+        subject: 'HI...',
+        text: 'That was easy!'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    
+})
+    app.post('/login',(req,res)=>{
     const {email,password}=req.body;
     User.findOne({email:email}).then(user=>{
         if(user){
@@ -64,7 +71,6 @@ app.post('/login',(req,res)=>{
 })
 
 
-app.post('/')
 
 
 
